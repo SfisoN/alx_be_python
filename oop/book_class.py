8054ab66ceeab7fb4512851b3b8a227b4321f41c
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-10-10 21:35:46 +0200

    new update

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
         self.year = year
@@ -8,7 +8,7 @@ class Book:
         print(f"Deleting {self.title}")
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} ({self.year})"
+        return f"'{self.title}' by {self.author}, published in ({self.year})"
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book({self.title!r}, {self.author!r}, {self.year})"
